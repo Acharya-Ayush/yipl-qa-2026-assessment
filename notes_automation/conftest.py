ef04034e374530cb7697b6a4c39860a2af517e60
Ayush Acharya <49689971+Acharya-Ayush@users.noreply.github.com>
2026-02-11 02:15:56 +0545

    Initialization_and_crud_operation. (#2)
    * Signup and login test case.
    
    * CRUD operation test.

--- notes_automation/conftest.py
+++ notes_automation/conftest.py
@@ -1,14 +1,36 @@
 import pytest
 from playwright.sync_api import sync_playwright
+from pages.login_page import LoginPage
+from utils.config import EMAIL, PASSWORD, BASE_URL
 
-@pytest.fixture(scope="function")
-def page():
+@pytest.fixture(scope="session")
+def browser():
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False)
-        context = browser.new_context(viewport=None)  # tells Playwright to use full window
-        page = context.new_page()
-        
-        # Get the screen size (use typical large values if unsure)
-        page.set_viewport_size({"width": 1920, "height": 1080})  # manually maximize
-        yield page
+        yield browser
         browser.close()
+
+@pytest.fixture(scope="session")
+def logged_in_page(browser):
+    context = browser.new_context(viewport=None)
+    page = context.new_page()
+
+    # Manual maximize by setting viewport size
+    page.set_viewport_size({"width": 1920, "height": 1080})
+
+    login_page = LoginPage(page)
+    login_page.navigate(BASE_URL)
+    login_page.login()
+    yield page
+    context.close()
+
+@pytest.fixture(scope="function")
+def page(browser):
+    context = browser.new_context(viewport=None)
+    page = context.new_page()
+
+    # Manual maximize by setting viewport size
+    page.set_viewport_size({"width": 1920, "height": 1080})
+
+    yield page
+    context.close()
--- notes_automation/tests/smoke/test_signup_and_login.py
+++ notes_automation/tests/smoke/test_signup_and_login.py
@@ -3,11 +3,11 @@ from pages.signup_page import SignupPage
 from pages.login_page import LoginPage
 from utils.config import BASE_URL
 
-@pytest.mark.smoke
+@pytest.mark.smoke_independent()
 def test_valid_signup_and_login(page):
     signup_page = SignupPage(page)
     signup_page.navigate()
     signup_page.signup()
     login_page = LoginPage(page)
     login_page.navigate(BASE_URL)
-    login_page.login()  # uses credentials from .env
+    login_page.login()  
